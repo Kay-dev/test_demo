4b0f59afbce99bf087f8a8e006843814293aa19e
zhengwei <a932327192@gmail.com>
2022-06-10 10:52:09 +0800

    init

---
+++ src/test/java/com/example/spring_test_demo/SpringTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_test_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
